06692595672140d64306f0434a99ad280748c46e
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-19 23:19:43 +0530

    Add loopsBasic.py with examples of while loop and for loop

---
+++ Chapter-6/loopsBasic.py
@@ -0,0 +1,10 @@
+# loop :- agin and agin process same things 
+# two types of loop:-  while loop and for loop
+
+num = 1 
+
+while num<=100:
+    print("Payal Kumari")
+    num= num+1
+
+print("Now we are but of the while loop")    
