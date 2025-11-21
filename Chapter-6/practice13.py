c9da3961344e7f734d80ec32f92c5ad2c9fa7e98
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-22 01:08:59 +0530

    Add practice script to print username five times in uppercase letters in Chapter 6

---
+++ Chapter-6/practice13.py
@@ -0,0 +1,13 @@
+# 13.Saumya wants to print her username five times in uppercase letters.
+# Write a program to print:
+# PAYALKUMARI
+# PAYALKUMARI
+# PAYALKUMARI
+# PAYALKUMARI
+# PAYALKUMARI
+# Hint:
+# Use a simple for loop and .upper() method.
+
+for i in range(5):
+    print("PayalKuamri".upper())
+    
