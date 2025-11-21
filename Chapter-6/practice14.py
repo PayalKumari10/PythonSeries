a19cea410d567279209f0bcc75a439229d320c78
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-22 01:13:08 +0530

    Add practice scripts for printing favorite foods and countries in Chapter 6

---
+++ Chapter-6/practice14.py
@@ -0,0 +1,8 @@
+# 14.You are given a list of Payal's favorite foods. Write a Python program to
+# print each food item using a for loop.
+
+favorite_foods = ["Pizza", "Burger", "Pasta", "Ice Cream", "Sushi"]
+
+for food in favorite_foods:
+    print(food)
+    
---
+++ Chapter-6/practice15.py
@@ -0,0 +1,10 @@
+# 15.Payal has created a tuple of countries she has already traveled to. Write a
+# Python program to print each country using a for loop.
+# countries_traveled = ("Malaysia", "Vietnam", "Switzerland", "Italy", "Bhutan")
+
+
+countries_traveled = ("Malaysia", "Vietnam", "Switzerland", "Italy", "Bhutan")
+
+for country in countries_traveled:
+    print(country)
+    
