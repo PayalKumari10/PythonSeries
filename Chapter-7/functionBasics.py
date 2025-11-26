ccfaa8c3fdb077278178233d41844206260eecac
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-26 22:56:24 +0530

    Add functionBasics.py with initial function examples and comments

---
+++ Chapter-7/functionBasics.py
@@ -0,0 +1,18 @@
+#Function Basic 
+
+# a = 3
+# b = 8
+# sum = a + b
+# print(sum)
+
+# some more lines of code
+
+# average= (a + b) / 2
+# print(average)
+
+# ..... 100 lines of code
+
+# a = 3
+# b = 8
+# sum = a + b
+# print(sum)
