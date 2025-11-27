fd2220f7730e9ba77a87eceff765a36cc9d7706d
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-27 15:17:46 +0530

    Add practice scripts for displaying Python messages and topics in Chapter 7

---
+++ Chapter-7/practice5.py
@@ -0,0 +1,5 @@
+# Write a function display_python() that prints "Python is Fun!"
+
+def display_python() :
+    print("Python is Fun!")
+display_python()    
---
+++ Chapter-7/practice6.py
@@ -0,0 +1,12 @@
+# Create a function learn() that prints three Python topics.
+
+def Learn() :
+    print("1. Functions")
+    print("2. Loops")
+    print("3. Data Structures")
+Learn()    
+
+
+# Explain what happense if you call a function before defining it.
+
+# If you call a function before defining it, Python will raise a NameError indicating that the function is not defined. This is because Python executes code sequentially, and it needs to know about the function's existence before it can be called.
