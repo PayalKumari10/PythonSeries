6f3429de516e91a9de6b8ea842ec47e81e35c5f6
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-27 16:17:14 +0530

    Add functions to count vowels and consonants in a string

---
+++ Chapter-7/practice11.py
@@ -0,0 +1,19 @@
+def countVowConso(userInput):
+
+    vowels = "aeiouAEIOU"
+    countVowels = 0
+    countConsonants = 0
+
+    for eachChar in userInput:
+        if eachChar.isalpha():
+            if eachChar in vowels:
+                countVowels += 1
+            else:
+                countConsonants += 1
+
+    return countVowels, countConsonants   # <-- this was missing
+
+
+# Function Call                
+vowels, consonants = countVowConso("Hello World")
+print(vowels, consonants)
