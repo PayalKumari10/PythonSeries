d749d671ed0bc9af09767290794af0d980ed5f2a
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-28 21:54:12 +0530

    Add convert_to_upper function to return uppercase version of a string

---
+++ Chapter-7/practice12.py
@@ -0,0 +1,6 @@
+#Define a function convert_to_upper(word) that returns the uppercase version of the string.
+
+def convert_to_upper(word):
+    return word.upper()
+#Example usage:
+print(convert_to_upper("hello"))  # Output: "HELLO"
