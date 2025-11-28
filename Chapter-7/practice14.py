7f2b5bf96e93ff10f7a2a681e4c2399fffba3596
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-29 00:12:35 +0530

    Add iterative and recursive factorial functions for comparison

--- Chapter-7/practice14.py
+++ Chapter-7/practice14.py
@@ -22,4 +22,20 @@ def fibonacci(n, a=0, b=1):
     if n > 0:
         print(a, end=' ')
         fibonacci(n - 1, b, a + b)
-print(fibonacci(7))        
+print(fibonacci(7))      
+
+
+
+# Write both a recursive and iterative function to find factorial - compare results.
+def iterative_factorial(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+def recursive_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * recursive_factorial(n - 1)
+print(iterative_factorial(5))
+print(recursive_factorial(5))
