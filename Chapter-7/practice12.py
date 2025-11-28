5ee49717903549226f407be9d56b8a48ad53fbc6
PayalKumari10 <kumaripayal7488@gmail.com>
2025-11-28 23:50:33 +0530

    Add message function to return a customizable message with a default value

--- Chapter-7/practice12.py
+++ Chapter-7/practice12.py
@@ -4,3 +4,10 @@ def convert_to_upper(word):
     return word.upper()
 
 print(convert_to_upper("hello")) 
+
+#Define a function message(text="Keep Learning!") and call it with and without an argument.
+
+def message(text="Keep Learning!"):
+    return text
+print(message())
+print(message("Stay Positive!"))
