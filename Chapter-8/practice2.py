9eb3b8d5d5e0b0c1317f7895517ffc25398c5abc
PayalKumari10 <kumaripayal7488@gmail.com>
2025-12-07 20:35:48 +0530

    update file "practice2.py"

--- Chapter-8/practice2.py
+++ Chapter-8/practice2.py
@@ -1,6 +1,10 @@
 # Open a file called report.txt in write mode.
 
-file= open("report.txt", "w", encoding="utf-8")
+# file= open("report.txt", "w", encoding="utf-8")
+
+#write in append mode
+file= open("report.txt", "a", encoding="utf-8")
+file.write("All Set")
 
 file.write("Kafi sahi se Python Sikh rahe hai. Maja aaraha hai \n")
 file.write("File handling bhi seekh liya hai. \n")
